a2655986bc793299df2d6bbd7dadeb002865da5c
Kevin Luo <kvn.luo@gmail.com>
2020-10-22 15:52:14 -0400

    env file

--- public/erc_fxns.js
+++ public/erc_fxns.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql');
 const axios = require('axios');
+require('dotenv').config();
 
 const db = mysql.createPool({
   connectionLimit: 10,
   host:'localhost',
   port:3306,
   user:'root',
-  password:'h0opDr3@m$',
+  password:process.env.mysqlP,
   database:'etherscan_db'
 })
 
